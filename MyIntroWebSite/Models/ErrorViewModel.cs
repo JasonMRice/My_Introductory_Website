b32db0d3f522c4a9c0e77c4a076575ab1a32bad0
Jason Rice <jasonmatthew2352@gmail.com>
2020-01-02 16:15:42 -0600

    Uploading the initial, blank, ASP.Net Core MVC web app using EF Core 3.1

---
+++ MyIntroWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyIntroWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
